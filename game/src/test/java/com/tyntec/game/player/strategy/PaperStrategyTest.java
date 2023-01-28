c6814d9a254e9725b05bbde25a3522ada70c8059
Marko Milic <marko.milic@htecgroup.com>
2023-01-28 23:09:39 +0100

    Add PaperStrategyTest

---
+++ game/src/test/java/com/tyntec/game/player/strategy/PaperStrategyTest.java
@@ -0,0 +1,29 @@
+package com.tyntec.game.player.strategy;
+
+import com.tyntec.move.Move;
+import com.tyntec.move.Paper;
+import com.tyntec.player.strategy.PaperStrategy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaperStrategyTest {
+
+  private PaperStrategy paperStrategy;
+
+  @BeforeEach
+  void setup() {
+    paperStrategy = new PaperStrategy();
+  }
+
+  @Test
+  void should_returnPaper_when_getMoveIsCalled() {
+    // Given
+
+    // When
+    Move move = paperStrategy.getMove();
+
+    // Then
+    Assertions.assertEquals(Paper.class, move.getClass());
+  }
+}
